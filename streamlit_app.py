af7e57b33c0d90c368853d50f2d8489866e97b56
mayanksnowflake <140278021+mayanksnowflake@users.noreply.github.com>
2023-07-22 21:36:36 +0100

    Update streamlit_app.py
    Added Pictures Against The Breakfast Menu - UX

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.title('My Parents New Healthy Diner!')
 st.title('Healthy Everyday Menu_italics_ :blue[colors] and emojis :sunglasses:')
 
-st.header ('Breakfast Menu')
-st.text('Omega 3 & Bluberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard Boiked Free Range Eggs')
+st.header ('🍞 Breakfast Menu')
+st.text('🥣 Omega 3 & Bluberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard Boiked Free Range Eggs')
 
 
