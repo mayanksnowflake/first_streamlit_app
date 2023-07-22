c9f4004dc6d4bdabf8a5be66247229ad5d997c52
mayanksnowflake <140278021+mayanksnowflake@users.noreply.github.com>
2023-07-22 21:34:09 +0100

    Update streamlit_app.py
    Added more text to the web page - breakfast menu updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 st.title('My Parents New Healthy Diner!')
 st.title('Healthy Everyday Menu_italics_ :blue[colors] and emojis :sunglasses:')
+
+st.header ('Breakfast Menu')
+st.text('Omega 3 & Bluberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard Boiked Free Range Eggs')
+
+
